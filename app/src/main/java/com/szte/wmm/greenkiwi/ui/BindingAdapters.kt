1d3422a9a32103c755b1c92e6a6603d508546a99
Matensz <wolf.mate.mark@gmail.com>
2020-10-04 13:41:56 +0200

    added glide dependency, added image by url loading binding adapter for the activity list items

--- app/src/main/java/com/szte/wmm/greenkiwi/ui/BindingAdapters.kt
+++ app/src/main/java/com/szte/wmm/greenkiwi/ui/BindingAdapters.kt
@@ -1,7 +1,10 @@
 package com.szte.wmm.greenkiwi.ui
 
+import android.widget.ImageView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.szte.wmm.greenkiwi.repository.domain.Activity
 import com.szte.wmm.greenkiwi.ui.activities.ActivityAdapter
 
@@ -10,5 +13,15 @@ fun bindRecyclerView(recyclerView: RecyclerView, data: List<Activity>?) {
     val adapter = recyclerView.adapter as ActivityAdapter
     adapter.submitList(data) {
         recyclerView.scrollToPosition(0)
+    }
+}
+
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .into(imgView)
     }
 }
