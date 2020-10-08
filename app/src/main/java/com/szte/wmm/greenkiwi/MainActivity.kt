c2e35423d170ad08f15615ca0b57c5ed66eb4938
Matensz <wolf.mate.mark@gmail.com>
2020-10-08 23:27:19 +0200

    changed the default style, removed default app bar from the app layout, minor refactors in project gradle file

--- app/src/main/java/com/szte/wmm/greenkiwi/MainActivity.kt
+++ app/src/main/java/com/szte/wmm/greenkiwi/MainActivity.kt
@@ -3,8 +3,6 @@ package com.szte.wmm.greenkiwi
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -16,11 +14,6 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_activities, R.id.navigation_notifications, R.id.navigation_settings))
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
