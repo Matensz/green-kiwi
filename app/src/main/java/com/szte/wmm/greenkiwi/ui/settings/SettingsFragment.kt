0272eb65598234cc9916f182ae3ee090d78c1670
Matensz <wolf.mate.mark@gmail.com>
2020-10-25 22:20:10 +0100

    added instructions button to the settings fragment

--- app/src/main/java/com/szte/wmm/greenkiwi/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/szte/wmm/greenkiwi/ui/settings/SettingsFragment.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.szte.wmm.greenkiwi.R
+import com.szte.wmm.greenkiwi.ui.instructions.InstructionsDialogProvider
 
 class SettingsFragment : Fragment() {
 
@@ -22,10 +22,10 @@ class SettingsFragment : Fragment() {
         settingsViewModel =
             ViewModelProviders.of(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
-        val textView: TextView = root.findViewById(R.id.text_settings)
-        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val instructionsButton: Button = root.findViewById(R.id.instructions_button)
+        instructionsButton.setOnClickListener {
+            InstructionsDialogProvider.createInstructionsDialog(requireActivity()).show()
+        }
         return root
     }
 }
