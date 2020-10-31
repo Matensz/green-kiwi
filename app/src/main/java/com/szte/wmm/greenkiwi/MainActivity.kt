44e8b8b780df2d23881e1d39cc589746c2656945
Matensz <wolf.mate.mark@gmail.com>
2020-10-31 20:39:50 +0100

    implemented night mode switch in settings

--- app/src/main/java/com/szte/wmm/greenkiwi/MainActivity.kt
+++ app/src/main/java/com/szte/wmm/greenkiwi/MainActivity.kt
@@ -3,6 +3,7 @@ package com.szte.wmm.greenkiwi
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -11,6 +12,7 @@ import com.szte.wmm.greenkiwi.ui.instructions.InstructionsDialogProvider
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        initNightModeSettings()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
@@ -26,5 +28,12 @@ class MainActivity : AppCompatActivity() {
     private fun isReturningUser(): Boolean {
         val sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE) ?: return true
         return sharedPref.getBoolean(getString(R.string.returning_user_key), false)
+    }
+
+    private fun initNightModeSettings() {
+        val sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE) ?: return
+        if (sharedPref.getBoolean(getString(R.string.night_mode_setting_key), false)) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
     }
 }
--- app/src/main/java/com/szte/wmm/greenkiwi/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/szte/wmm/greenkiwi/ui/settings/SettingsFragment.kt
@@ -1,31 +1,57 @@
 package com.szte.wmm.greenkiwi.ui.settings
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.switchmaterial.SwitchMaterial
 import com.szte.wmm.greenkiwi.R
 import com.szte.wmm.greenkiwi.ui.instructions.InstructionsDialogProvider
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SettingsFragment : Fragment() {
 
     private lateinit var settingsViewModel: SettingsViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        settingsViewModel =
-            ViewModelProviders.of(this).get(SettingsViewModel::class.java)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val application = requireNotNull(activity).application
+        val sharedPref = application.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+        val nightModeSettingKey = getString(R.string.night_mode_setting_key)
+
+        settingsViewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
+
         val instructionsButton: Button = root.findViewById(R.id.instructions_button)
         instructionsButton.setOnClickListener {
             InstructionsDialogProvider.createInstructionsDialog(requireActivity()).show()
         }
+
+        val nightModeSwitch: SwitchMaterial = root.findViewById(R.id.night_mode_switch)
+        val nightMode = sharedPref.getBoolean(nightModeSettingKey, false)
+        nightModeSwitch.isChecked = nightMode
+        nightModeSwitch.setOnCheckedChangeListener{ _, isChecked ->
+            val mode = if (isChecked) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+            AppCompatDelegate.setDefaultNightMode(mode)
+            lifecycleScope.launch {
+                saveNightModeSetting(sharedPref, isChecked, nightModeSettingKey)
+            }
+        }
+
         return root
+    }
+
+    private suspend fun saveNightModeSetting(sharedPref: SharedPreferences, isChecked: Boolean, nightModeSettingKey: String) {
+        withContext(Dispatchers.IO) {
+            sharedPref.edit().putBoolean(nightModeSettingKey, isChecked).apply()
+        }
     }
 }
