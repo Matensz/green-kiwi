49b1271b7337201886645a8aaf5f236b1bf30286
Matensz <wolf.mate.mark@gmail.com>
2020-10-31 13:57:25 +0100

    added util file with datetime related functions

---
+++ app/src/main/java/com/szte/wmm/greenkiwi/util/DateTimeUtils.kt
@@ -0,0 +1,28 @@
+package com.szte.wmm.greenkiwi.util
+
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun Long.isDayBeforeDate(millis: Long): Boolean {
+    val cal1: Calendar = Calendar.getInstance()
+    val cal2: Calendar = Calendar.getInstance()
+    cal1.timeInMillis = this
+    cal2.timeInMillis = millis
+    return cal1.get(Calendar.DAY_OF_YEAR) < cal2.get(Calendar.DAY_OF_YEAR) &&
+            cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR)
+}
+
+fun Long.isSameDay(millis: Long): Boolean {
+    val cal1: Calendar = Calendar.getInstance()
+    val cal2: Calendar = Calendar.getInstance()
+    cal1.timeInMillis = this
+    cal2.timeInMillis = millis
+    return cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR) &&
+            cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR)
+}
+
+@SuppressLint("SimpleDateFormat")
+fun formatDateString(lastAddedTimeStamp: Long?, defaultValue: String): String {
+    return lastAddedTimeStamp?.let { timeStamp -> SimpleDateFormat("yyyy-MM-dd").format(timeStamp).toString()} ?: defaultValue
+}
